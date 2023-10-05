7ba64ca05f62e970c5cdc704af932c13688fa1b0
Aldrich <106362981+AldrichSavin@users.noreply.github.com>
2023-10-05 15:59:36 +0800

    Pause delete

--- src/content/appliers/util.ts
+++ src/content/appliers/util.ts
@@ -1,3 +1,4 @@
+import { getWidgetToolsPanel, getWidgetToolsPosition, hideWidgetTools, showWidgetTools } from "../dom";
 import { UNIQUE_SCOPED_ID, EXTENSION_PREFIX } from "./constant";
 import { applierStore, getKeys } from "./store";
 
@@ -10,64 +11,62 @@ export function createApplierOptions(options = {}) {
         elementProperties: {
           // @ts-ignore
           ...(options.elementProperties || {}),
+
           onmouseover: function(event: MouseEvent) {
             const element = event.target as HTMLElement;
             element.style.cursor = "pointer";
 
-            /**
-             * 如果是删除按钮，不做处理
-             */
-            const isDelTarget = element.classList.contains("bookmark__highlighter__delete-button");
-            /**
-             * 如果已经有删除按钮，不做处理
-             */
-            const delButton = element.querySelector(".bookmark__highlighter__delete-button");
-            if (delButton || isDelTarget) {
-              return;
-            }
+            const widgetToolsPanel = getWidgetToolsPanel();
+            showWidgetTools(widgetToolsPanel, {
+              x: event.clientX,
+              y: event.clientY,
+              ...getWidgetToolsPosition(widgetToolsPanel),
+            });
 
-            // 增加一个删除按钮
-            const deleteButton = document.createElement("button");
-            deleteButton.innerText = "删除";
-            deleteButton.style.marginLeft = "4px";
-            deleteButton.style.cursor = "pointer";
-            deleteButton.style.color = "white";
-            deleteButton.style.fontSize = "12px";
+            // /**
+            //  * 如果是删除按钮，不做处理
+            //  */
+            // const isDelTarget = element.classList.contains("bookmark__highlighter__delete-button");
+            // /**
+            //  * 如果已经有删除按钮，不做处理
+            //  */
+            // const delButton = element.querySelector(".bookmark__highlighter__delete-button");
+            // if (delButton || isDelTarget) {
+            //   return;
+            // }
 
-            // 高度80% 垂直居中
-            deleteButton.style.height = "80%";
-            deleteButton.style.verticalAlign = "middle";
-
-            deleteButton.style.backgroundColor = "red";
-
-            deleteButton.classList.add("bookmark__highlighter__delete-button");
-            deleteButton.addEventListener("click", function(event: MouseEvent) {
-              const parent = element.parentNode;
-              if (parent) {
-                deleteButton.remove();
-                const textNode = document.createTextNode(element.innerText);
-                parent.replaceChild(textNode, element);
-              }
-            })
-            element.appendChild(deleteButton);
+            // // 增加一个删除按钮
+            // const deleteButton = document.createElement("button");
+            // deleteButton.innerText = "删除";
+            // deleteButton.style.cursor = "pointer";
+            // deleteButton.style.color = "white";
+            // deleteButton.style.fontSize = "12px";
+            // deleteButton.style.backgroundColor = "red";
+            // deleteButton.classList.add("bookmark__highlighter__delete-button");
+            // deleteButton.addEventListener("click", function(event: MouseEvent) {
+            //   const parent = element.parentNode;
+            //   if (parent) {
+            //     deleteButton.remove();
+            //     const textNode = document.createTextNode(element.innerText);
+            //     parent.replaceChild(textNode, element);
+            //   }
+            // })
+            // element.appendChild(deleteButton);
           },
-
           onmouseout: function(event: MouseEvent) {
-            const element = event.target as HTMLElement;
-            if(element.classList.contains("bookmark__highlighter__delete-button")) {
-              return;
-            }
-
-            element.style.cursor = "auto";
-
-            const delButton = element.querySelector(".bookmark__highlighter__delete-button");
-            if (delButton && delButton.contains(event.relatedTarget as HTMLElement)) {
-              return;
-            }
-
-            if (delButton) {
-              delButton.remove();
-            }
+            // const element = event.target as HTMLElement;
+            // if(element.classList.contains("bookmark__highlighter__delete-button")) {
+            //   return;
+            // }
+            // element.style.cursor = "auto";
+            // const delButton = element.querySelector(".bookmark__highlighter__delete-button");
+            // if (delButton && delButton.contains(event.relatedTarget as HTMLElement)) {
+            //   return;
+            // }
+            // if (delButton) {
+            //   delButton.remove();
+            // }
+            // hideWidgetTools();
           }
         },
     }
